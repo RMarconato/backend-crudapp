ab07685f4aad3aeadd9454da9cb8d9bf5a8533e6
Rafael <rafaelbmarconato@gmail.com>
2022-02-15 18:25:13 -0300

    fixing cleanup issue

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { FavoritesController } from './favorites.controller';
 import FavoritesDaRepository from './favorites.da.repository';
 import { FavoritesService } from './favorites.service';
 
 @Module({
-  controllers: [FavoritesController],
   providers: [FavoritesService, FavoritesDaRepository],
 })
 export class FavoritesModule {}
