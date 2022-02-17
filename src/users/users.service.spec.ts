263d8efbeea22c19a710ccb465ff6c3e54ec0fb3
Rafael <rafaelbmarconato@gmail.com>
2022-02-17 18:29:27 -0300

    addin ut for user

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import mockUsers from '../mock/mockUsers.json';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -14,5 +15,19 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findUserByUserName', () => {
+    it('should find a user by its name', async () => {
+      const user = mockUsers[0];
+
+      const result = await service.findUserByUserName(user.userName);
+      expect(result).toEqual(user);
+    });
+
+    it('should return undefined if user does not exists ', async () => {
+      const result = await service.findUserByUserName('unknown user');
+      expect(result).toEqual(undefined);
+    });
   });
 });
