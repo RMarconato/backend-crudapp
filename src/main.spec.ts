ac8c01ac4ea782812565a97839f7528b84a28bf5
Rafael <rafaelbmarconato@gmail.com>
2022-02-17 18:20:07 -0300

    Adding test for main

---
+++ src/main.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { NestFactory } from '@nestjs/core';
+
+jest.mock('@nestjs/core');
+
+describe('Entry point test', () => {
+  const mockApp = {
+    useGlobalPipes: jest.fn(),
+    useGlobalFilters: jest.fn(),
+    enableCors: jest.fn(),
+    listen: jest.fn(),
+  };
+  beforeAll(() => {
+    (NestFactory.create as jest.Mock).mockResolvedValue(mockApp);
+  });
+
+  it('application should be defined', async () => {
+    await import('./main');
+
+    expect(NestFactory).toBeDefined();
+    expect(NestFactory.create).toHaveBeenCalledTimes(1);
+  });
+});
