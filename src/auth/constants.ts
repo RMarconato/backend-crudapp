3bf24227c4f85559e937ab4e1973619bcd9a7ebb
Rafael <rafaelbmarconato@gmail.com>
2022-02-18 19:00:05 -0300

    Adding postman collection

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,6 +1,4 @@
 export const jwtConstants = {
   secret: '3dcc5051-c02e-469e-aea0-41c10e40c522',
-  // expirationTime: '300s',
-  // ToDo: Return original expiration time
-  expirationTime: '86400s',
+  expirationTime: '300s',
 };
